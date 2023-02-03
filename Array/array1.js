fcaa4c5734f948038577095a842d5b2b9e08672b
Sonu Gupta <rg3029926@gmail.com>
2023-02-03 15:21:30 +0530

    arr

--- Array/array1.js
+++ Array/array1.js
@@ -8,8 +8,18 @@ console.log(arr[2])//6
 console.log(arr[3])//7
 console.log(arr[4])//5
 let time2=performance.now()
-console.log(time1-time2)//max - time
+console.log(time1-time2)//-4.913300007581711 - time
 
+//bettar 
 
+let time3=performance.now()
+for(let i=0; i<arr.length; i++){
+    
+    console.log(arr[i])
+    
+}
+
+let time4=performance.now()
+    console.log(time3-time4)// -1.1525000035762787-time
 
 
