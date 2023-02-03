92aff68503ca207049f19036a7746d9ae6135b27
Sonu Gupta <rg3029926@gmail.com>
2023-02-03 16:08:50 +0530

    array

--- Array/array2.js
+++ Array/array2.js
@@ -1,6 +1,8 @@
 
 //check target sumNumber
 
+const { boolean } = require("yargs");
+
 let arr=[3,4,6,7,8,3,4,5,3,6]
 let n=arr.length;
 let k=7;
@@ -22,6 +24,14 @@ let time2=performance.now()
 
 console.log(`run time ${time1-time2}`)//run time -3.9916000068187714
 
+
+// better 
+
+
+// The Set.has() method in javaScript is used to check whether an element 
+// whith a specified value exist in a Set or not . it return boolean
+// value .it return true if the element exsit on the set else it
+// return false if the element doesn't exist.
 function hasPairWithSum(arr, n, k){
     const mnset=new Set();
     for(let i=0; i<n; i++){
