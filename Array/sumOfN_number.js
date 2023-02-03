b119be70769938034c278f953e66070843c9ef68
Sonu Gupta <rg3029926@gmail.com>
2023-02-03 16:34:25 +0530

    nsum

--- Array/sumOfN_number.js
+++ Array/sumOfN_number.js
@@ -1,6 +1,6 @@
 let n=8;
 
-function sumOfnNumer(n){
+function sumOfnNumer1(n){
     let total=0;
     for(let i=0; i<=n; i++){
         total+=i
@@ -9,6 +9,22 @@ function sumOfnNumer(n){
 }
 let time1=performance.now()
 
-console.log(sumOfnNumer(n))
+console.log(sumOfnNumer1(n))
 
 let time2=performance.now()
+
+console.log(`run time ${time1-time2}`)//run time -5.44990000128746
+
+//better  
+// function sumOfnNumer2(n){
+//     return n* (n+1)/2
+// }
+
+function sumOfnNumer2(n){return n* (n+1)/2}
+let time3=performance.now()
+
+console.log(sumOfnNumer1(n))
+
+let time4=performance.now()
+
+console.log(`run time ${time3-time4}`)//run time -0.2158999890089035
