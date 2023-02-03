f4702e00fbee11a3a05f4e1961aab6c944cd5183
Sonu Gupta <rg3029926@gmail.com>
2023-02-03 15:15:02 +0530

    arr

---
+++ Array/array1.js
@@ -0,0 +1,15 @@
+
+let arr=[3,5,6,7,5]
+
+let time1=performance.now()
+console.log(arr[0])//3
+console.log(arr[1])//5
+console.log(arr[2])//6
+console.log(arr[3])//7
+console.log(arr[4])//5
+let time2=performance.now()
+console.log(time1-time2)//max - time
+
+
+
+
