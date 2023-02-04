9af4460c36d08ef04914f86256a065b052d98241
Sonu Gupta <rg3029926@gmail.com>
2023-02-04 13:42:55 +0530

    insert

--- Array/array_insert.js
+++ Array/array_insert.js
@@ -1,6 +1,14 @@
 let data=[4, 5, 7, 6, 8, 2, 9, 3, 5, 12, 32, 45];
 let newEle=70;
 let position=3;
-for(let i=0; i<data.length; i++){
+for(let i=data.length; i>=0; i--){
     console.log(data[i])
+    if(i>=position){
+        data[i+1]=data[i]
+        if(i==position){
+            data[i]=newEle;
+        }
+    }
+    
 }
+console.log(data)
