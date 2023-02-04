59c128bec0fb19c79791437a39b338d204ec496b
Sonu Gupta <rg3029926@gmail.com>
2023-02-04 14:20:25 +0530

    insert Array

--- Array/array_insert.js
+++ Array/array_insert.js
@@ -1,14 +1,20 @@
-let data=[4, 5, 7, 6, 8, 2, 9, 3, 5, 12, 32, 45];
-let newEle=70;
-let position=3;
-for(let i=data.length; i>=0; i--){
-    console.log(data[i])
-    if(i>=position){
-        data[i+1]=data[i]
-        if(i==position){
-            data[i]=newEle;
+
+function insertEle(){
+    let data=[4, 5, 7, 6, 8, 2, 9, 3, 5, 12, 32, 45];
+    let newEle=document.getElementById('newEle').value;
+
+    let position=document.getElementById('position').value;
+    for(let i=data.length; i>=0; i--){
+        console.log(data[i])
+        if(i>=position){
+            data[i+1]=data[i]
+            if(i==position){
+                data[i]=newEle;
+            }
         }
+        
     }
-    
+    let newdata=document.getElementById("el").innerHTML=data;
+    console.warn(`New Array = ${newdata.join(" ")}`)
+    console.log(data)
 }
-console.log(data)
