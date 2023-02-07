c5326f0bb027da1ef578fc220be01d3c49738534
Sonu Gupta <rg3029926@gmail.com>
2023-02-07 19:49:02 +0530

    even sum

---
+++ WhileLoop/evenSumBelowN.js
@@ -0,0 +1,13 @@
+function evenSumBelowN(num) {
+    // Write code here
+    let sum=0;
+  let i=1;
+    while(i<=num){
+        if(i%2==0){
+            sum+=i
+        }
+        i++;
+    }
+    console.log(sum)
+}
+evenSumBelowN(9)
