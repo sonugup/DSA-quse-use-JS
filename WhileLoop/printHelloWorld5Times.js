6d9cc94cd23a8e5e49d8e832a83328271811e20c
Sonu Gupta <rg3029926@gmail.com>
2023-02-07 19:24:40 +0530

    hello world

---
+++ WhileLoop/printHelloWorld5Times.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+// for(let i=1; i<=5; i++){
+//     console.log("Hello World")
+// }
