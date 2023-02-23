40e9124ad3e999a5e24e6f5fcce370335c7ef08b
Sonu Gupta <rg3029926@gmail.com>
2023-02-23 23:01:56 +0530

    dsa

--- Array/array_delete.js
+++ Array/array_delete.js
@@ -1,5 +1,5 @@
 
-// function deleteEle(){
+function deleteEle(){
     let data=[5, 8, 2, 9, 23, 45, 11, 85];
     let position=document.getElementById("position").value;
     position=parseInt(position)
@@ -10,4 +10,4 @@
     data.length=data.length-1;
     let newdata=document.getElementById("el").innerHTML=data;
     console.warn(newdata);
-// }
+}
