30a366295159c199ff0f91a1c2277aed1ff58814
Sonu Gupta <rg3029926@gmail.com>
2023-02-25 12:53:02 +0530

    buy ticket

--- Array/buy_ticket.js
+++ Array/buy_ticket.js
@@ -1,20 +1,22 @@
-var timeRequiredToBuy = function(tickets, k) {
-    let count = 0,i = 0;
-       while(tickets[k] > 0){
-           for(let j=0; j<tickets.length; j++){
-               if(tickets[j] > 0){
-                   tickets[j] -= 1;
-                   count++;
-               }
-               if(tickets[k] == 0){
-                   break;
-               }
-           }
-           i++;
-       }
-       return count;
+var timeRequiredToBuy =  (tickets, k) => {
+  let count = 0,
+    i = 0;
+  while (tickets[k] > 0) {
+    for (let j = 0; j < tickets.length; j++) {
+      if (tickets[j] > 0) {
+        tickets[j] -= 1;
+        count++;
+      }
+      if (tickets[k] == 0) {
+        break;
+      }
+    }
+    i++;
+  }
+  return count;
 };
 
-let tickets=[2,3,2]
-let k=2
-console.log(timeRequiredToBuy(tickets, k))
+
+let tickets = [2, 3, 2];
+let k = 2;
+console.log(timeRequiredToBuy(tickets, k));
