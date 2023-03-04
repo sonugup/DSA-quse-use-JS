9acfa7217071745b574d27df6a6f706407cf343e
Sonu Gupta <rg3029926@gmail.com>
2023-03-04 23:36:41 +0530

    Tow sum

--- Array/Tow_sum.js
+++ Array/Tow_sum.js
@@ -1,19 +1,17 @@
 // Tow Sum
-
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-var twoSum = function(nums, target) {
-
-
-    for(var i=0; i<nums.length; i++){
-        for(var j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j] == target){
-                return [i,j]
-            }
-        }
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
     }
+  }
 };
 
-let nums = [2,7,11,15], target = 9;
+let nums = [2, 7, 11, 15],
+  target = 9;
 
-console.log(twoSum(nums, target))
+console.log(twoSum(nums, target));
