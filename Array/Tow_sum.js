d7f729f5a8df86e8f4890beda1c90166905da607
Sonu Gupta <rg3029926@gmail.com>
2023-03-04 23:31:38 +0530

    Tow Sum

--- Array/Tow_sum.js
+++ Array/Tow_sum.js
@@ -1,11 +1,14 @@
-var twoSum = function(nums, target) {
-
-
-    for(var i=0; i<nums.length; i++){
-        for(var j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j] == target){
-                return [i,j]
-            }
-        }
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
     }
+  }
 };
+
+let nums = [2, 7, 11, 15],
+  target = 9;
+
+console.log(twoSum(nums, target));
