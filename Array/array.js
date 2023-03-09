c16a11f639f3588366262b7cb994dba7e0e9f503
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-09 22:27:35 +0530

    Update array.js

--- Array/array.js
+++ Array/array.js
@@ -3,8 +3,8 @@
 let b="";
 let arr=[2, 4, 6, 5, 7, 8, 3, 9];
 for(var i=0; i<arr.length; i++){
-  b+=arr[i]
+  b+=arr[i]*(Math.floor(Math.random()*10)) + " ";
  
 }
 
- console.log(b*(Math.floor(Math.random()*10)));
+ console.log(b);
