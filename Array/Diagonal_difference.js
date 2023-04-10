de34f3ac3406a78fd6de56944076dc1d8883adca
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-10 23:27:30 +0530

    Update Diagonal_difference.js

--- Array/Diagonal_difference.js
+++ Array/Diagonal_difference.js
@@ -1,1 +1,19 @@
 
+
+function diagonalDifference(arr) {
+    // Write your code here
+    let sum=0;
+    let sum2=0;
+for(let i=0; i<arr.length; i++){
+  for(let j=0; j<arr.length; j++){
+    if(i==j){
+      sum+= arr[i][i];
+    }
+    if((i+j)==(arr.length-1))
+    
+    sum2+=arr[i][j]
+  }
+}
+let sum1=Math.abs(sum2-sum)
+return sum1
+}
