6a7dddd48393ebbfb6a9665f990805b16bc667b4
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-10 23:01:11 +0530

    Create Mini-Max_sum.js

---
+++ Array/Mini-Max_sum.js
@@ -0,0 +1,14 @@
+// Given five positive integers, find the minimum and maximum values that 
+// can be calculated by summing exactly four of the five integers.
+// Then print the respective minimum and maximum values as a single 
+// line of two space-separated long integers.
+
+
+function miniMaxSum(arr) {
+    // Write your code here
+   arr.sort((a, b) => a - b);
+const minSum = arr.slice(0, 4).reduce((acc, curr) => acc + curr);
+const maxSum = arr.slice(1).reduce((acc, curr) => acc + curr);
+console.log(`${minSum} ${maxSum}`);
+}
+
