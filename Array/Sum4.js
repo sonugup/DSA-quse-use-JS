28da5a3d2798c432d22dae5f1d9a7a518442a18d
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-05-01 13:56:51 +0530

    Update Sum4.js

--- Array/Sum4.js
+++ Array/Sum4.js
@@ -15,3 +15,47 @@
 
 // Input: nums = [2,2,2,2,2], target = 8
 // Output: [[2,2,2,2]]
+
+
+var fourSum = function(nums, target) {
+    let ans=[];
+    let arr=nums.sort((a, b) => a-b)
+
+    for(let i=0; i< arr.length-3; i++){
+        if(i>0 && arr[i]===arr[i-1]){
+            continue
+        }
+        for(let j=i+1; j<arr.length-2; j++){
+            if(j> i+1 && arr[j]===arr[j-1]){
+                continue;
+            }
+
+            let l=j+1;
+            let r=arr.length-1;
+            while(l<r){
+                let sum=arr[i]+arr[j]+arr[l]+ arr[r]
+                if(sum===target){
+                    ans.push([arr[i], arr[j], arr[l], arr[r]])
+
+                    while(l<r && arr[l] === arr[l+1]){
+                    l++
+                }
+                while(l<r && arr[r] === arr[r-1]){
+                    r--
+                }
+                l++;
+                r--
+                }
+                
+                else if(sum<target){
+                    l++;
+                }
+                else{
+                    r--
+                }
+            }
+        }
+    }
+    return ans
+
+};
