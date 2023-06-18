d27b0955ac3f8a5766e8c501110e12ae80e5a7c3
Sonu Gupta <rg3029926@gmail.com>
2023-06-18 15:03:38 +0530

    squer Pattern

---
+++ Pattern/Pattern_Printing.js
@@ -0,0 +1,28 @@
+function patternPrinting(N) {
+    // Write code here
+   
+
+  for (var i=1; i<=N; i++) {
+      let pattern = '';
+    for(var j=1; j<=i; j++){
+        pattern+="*";
+    }
+    console.log(pattern)
+  }
+}
+
+patternPrinting(5)
+
+/*
+
+Input : 5;
+
+Output:
+*
+**
+***
+****
+*****
+
+*/
+
---
+++ Pattern/squer_Pattern.js
@@ -0,0 +1,18 @@
+
+function yourFirstPattern(n) {
+    // Write code here
+    
+    var i=1;
+    while(i<=n){
+        var bag="";
+        var j=1;
+        while(j<=n){
+            bag+="*"+" ";
+            j++;
+           
+        }
+         console.log(bag)
+        i++;
+        
+    }
+}
