457da5f3485a9823cfc86763eef4b168d6cfb453
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-06-18 17:13:57 +0530

    Tow prime Numbers

---
+++ Prime/Tow_primes.js
@@ -0,0 +1,24 @@
+
+function twoPrimes(a,b) {
+    //write code here
+   if (isPrime(a) && isPrime(b)) {
+  console.log('True');
+} else {
+  console.log("False");
+}
+
+}
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
