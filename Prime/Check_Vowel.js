15acc6ece5b11175b1f004e4cee668a96175d1d5
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-06-22 19:52:57 +0530

    Create Check_Vowel.js

---
+++ Prime/Check_Vowel.js
@@ -0,0 +1,15 @@
+function checkVowel(N, str) {
+  //write code here
+  let c=0
+  for(var i=0; i<N; i++){
+      if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u"){
+          c++
+      }
+  }
+  if(c>0){
+          console.log(true)
+      }else{
+          console.log(false)
+      }
+  
+}
