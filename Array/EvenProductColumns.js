eb4e7ca1554cf5426777434109dcd6696d17f0d0
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-07-06 21:06:02 +0530

    Update EvenProductColumns.js

--- Array/EvenProductColumns.js
+++ Array/EvenProductColumns.js
@@ -1,1 +1,14 @@
 
+function evenProductInColumns(N,M, arr){
+    //write code here
+    for(var i=0; i<M; i++){
+        let mul=1
+        for(var j=0; j<N; j++){
+            if(arr[j][i]%2==0){
+                mul=mul*arr[j][i]
+            }
+        }
+        console.log(mul)
+    }
+  
+}
