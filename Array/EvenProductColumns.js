84d9716bb389c3bbf430c816eb9f9ba35fd290d1
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-07-06 21:10:08 +0530

    EvenProductColumns

--- Array/EvenProductColumns.js
+++ Array/EvenProductColumns.js
@@ -12,3 +12,15 @@ function evenProductInColumns(N,M, arr){
     }
   
 }
+
+// output:
+let n=3;
+let m=2;
+let arr=[[1,2,3],[4, 5, 6],[7, 8, 9]]
+
+evenProductInColumns(n, m, arr)
+
+// input:
+// 4
+// 16
+// 6
