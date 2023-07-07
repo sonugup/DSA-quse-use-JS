164ef5f03bc690f5845a4c13c936623113c0ad20
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-07-07 18:51:44 +0530

    Update Sub_array_quest.js

--- Array/Sub_array_quest.js
+++ Array/Sub_array_quest.js
@@ -1,2 +1,24 @@
 
 Sub array quest II
+
+
+function subArrQueII(N, A) {
+  //write code here
+  var count=0
+  for(var i=0; i<N; i++){
+      
+      for(var j=i; j<N; j++){
+          let b=[]
+          for(var k=i; k<=j; k++){
+              b.push(A[k])
+             
+          } 
+          if(b[0]%2!=1 && b[b.length-1]%2!=1)count++
+        //   console.log(b)
+      }
+      
+  }
+  console.log(count)
+}
+
+let arr = [1, 2, 3, 4];
