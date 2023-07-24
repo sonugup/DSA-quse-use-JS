5e3e7a304548113c8d185f737ab219be37f07c35
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-07-24 21:10:47 +0530

    Update ProductOfArray.js

--- Array/ProductOfArray.js
+++ Array/ProductOfArray.js
@@ -1,1 +1,21 @@
 Product Of Array
+
+
+function Product_Of_Array(t, n, arr){
+   
+   const output = [];
+  let product = 1;
+
+  for (let i = 0; i < n; i++) {
+    output[i] = product;
+    product *= arr[i];
+  }
+
+  product = 1;
+ for (let i = n - 1; i >= 0; i--) {
+    output[i] *= product;
+    product *= arr[i];
+  }
+
+  console.log(output.join(" "))
+}
