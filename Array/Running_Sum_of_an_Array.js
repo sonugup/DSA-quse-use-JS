8bf574fa2198bf21e40924d8ad29c5bf149987cf
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-07-24 20:43:12 +0530

    Update Running_Sum_of_an_Array.js

--- Array/Running_Sum_of_an_Array.js
+++ Array/Running_Sum_of_an_Array.js
@@ -1,6 +1,6 @@
 Running_Sum_of_an_Array
 
-function Running_Sum_of_an_Array(t, n, arr){
+function Running_Sum_of_an_Array( n, arr){
     let bag="";
     let sum=0;
     for(let i=0; i<n; i++){
@@ -11,3 +11,8 @@ function Running_Sum_of_an_Array(t, n, arr){
     }
     console.log(bag)
 }
+
+let n=5;
+let arr=[1, 2, 3, 4, 5];
+
+Running_Sum_of_an_Array( n, arr)
