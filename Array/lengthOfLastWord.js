69096a16c35f97a10707fa8f01ba617fdbc41a9b
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-09-07 17:08:34 +0530

    Length Of Last Word

--- Array/lengthOfLastWord.js
+++ Array/lengthOfLastWord.js
@@ -1,4 +1,6 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
 
+// A word is a maximal substring consisting of non-space characters only.
 
 var lengthOfLastWord = function(s) {
     if(s == ""){
@@ -8,3 +10,9 @@ var lengthOfLastWord = function(s) {
     var LastWord = nArr[nArr.length -1];
     return LastWord.length;
 };
+
+
+// Input: 
+var str = "Hello World"
+// Output: 5
+lengthOfLastWord(str)
