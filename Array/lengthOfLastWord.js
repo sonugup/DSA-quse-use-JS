c3337d477c37464f19356e70f7e5beb7c93f4fde
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-09-07 17:17:24 +0530

    Length Of Last Word

--- Array/lengthOfLastWord.js
+++ Array/lengthOfLastWord.js
@@ -2,6 +2,7 @@
 
 // A word is a maximal substring consisting of non-space characters only.
 
+// mathor-1
 var lengthOfLastWord = function(s) {
     if(s == ""){
         return 0;
@@ -11,8 +12,36 @@ var lengthOfLastWord = function(s) {
     return LastWord.length;
 };
 
+// mathor-2
+var lengthOfLastWord = function(str) {
+   let count = 0;
+    let flag = false;
+ 
+    // Loop through the string backwards
+    for (let i = str.length - 1; i >= 0; i--) {
+        // Once the first character from last
+        // is encountered, set char_flag to true.
+        if ((str[i] >= 'a' && str[i] <= 'z')
+            || (str[i] >= 'A' && str[i] <= 'Z')) {
+            flag = true;
+            count++;
+        }
+        // When the first space after the
+        // characters (from the last) is
+        // encountered, return the length
+        // of the last word
+        else {
+            if (flag == true)
+                return count;
+        }
+    }
+ 
+    return count;
+};
 
 // Input: 
 var str = "Hello World"
 // Output: 5
 lengthOfLastWord(str)
+
+cosnorl.log(lengthOfLastWord(str))
