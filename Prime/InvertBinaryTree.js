9fa66eb6fcf8e1654b96b373be0a753ee0e48e81
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-10-13 14:20:18 +0530

    Invert Binary Tree

--- Prime/InvertBinaryTree.js
+++ Prime/InvertBinaryTree.js
@@ -1,3 +1,11 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+
 const invertTree = function(root) {
     if(root===null){
         return null
@@ -12,3 +20,6 @@ const invertTree = function(root) {
 
     return root
 };
+
+let  root = [4,2,7,1,3,6,9];
+console.log(invertTree(root);
