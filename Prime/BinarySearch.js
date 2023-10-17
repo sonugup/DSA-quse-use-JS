957f3f5053a8425a7f06e2162d1a1e06acee10d0
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-10-17 16:03:13 +0530

    Update BinarySearch.js

--- Prime/BinarySearch.js
+++ Prime/BinarySearch.js
@@ -1,1 +1,18 @@
-
+var search = function(nums, target) {
+    let i=0;
+    let j=nums.length-1;
+    while(i<=j){
+        let mid= i+ Math.floor((j-i)/2)
+        if(nums[mid] == target){
+            return mid;
+        
+        }
+        else if(nums[mid]>target){
+            j=mid-1;
+        }else{
+            i=mid+1;
+        }
+        
+    }
+    return -1
+};
