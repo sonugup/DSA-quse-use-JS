96e9a66cb4aba25d3a9476ce04a17e153ff947a6
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-10-17 16:04:12 +0530

    Update BinarySearch.js

--- Prime/BinarySearch.js
+++ Prime/BinarySearch.js
@@ -1,3 +1,9 @@
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+
 var search = function(nums, target) {
     let i=0;
     let j=nums.length-1;
