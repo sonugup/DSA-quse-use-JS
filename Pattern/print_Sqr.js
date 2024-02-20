1bb24cbf06911eb16b7207c107818b496f2a7ce4
sonugup <skg30336@gmail.com>
2024-02-20 19:02:38 +0530

    add file

---
+++ Pattern/print_Sqr.js
@@ -0,0 +1,19 @@
+
+const print_sqr =(n) => {
+    let char=["a", "b", "c", "d"]
+    let i=1;
+    while(i<=n){
+        let ans="";
+        let j=1;
+        while(j<=n){
+        ans+=i+j-1+" ";
+
+            j++;
+        }
+        i++;
+        console.log(ans)
+    }
+}
+const n=4;
+
+print_sqr(n)
