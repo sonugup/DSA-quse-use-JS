20fb49f40abd1554492e565298d3d77d082f646b
sonugup <skg30336@gmail.com>
2024-03-10 15:43:56 +0530

    Z Traversal

--- Matrix/Z_traversal.js
+++ Matrix/Z_traversal.js
@@ -1,7 +1,22 @@
 
-
+const Z_traversal = (mtr) => {
+   let bag="";
+   let n=mtr.length;
+   for(var j=0; j<n; j++){
+    bag+=mtr[0][j]+" ";
+    // console.log(bag)
+   }
+   for(var j=n-2; j>=0; j--){
+    bag+=mtr[ n-1-j][j]+" ";
+    // console.log(bag)
+   }
+   for(var j=1; j<n; j++){
+    bag+=mtr[n-1][j]+" ";
+   }
+console.log(bag)
+}
 let mtr = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]]
- N_traversal(mtr)
+ Z_traversal(mtr)
